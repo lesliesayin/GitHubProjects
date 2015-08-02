10b4cf6a938cfc4f271cc8140877f189bdc13729
Leslie Anne Sayin <sayin.leslieanne@gmail.com>
2015-08-03 02:21:24 +0800

    With Kira

--- SampleProject/src/Main.java
+++ SampleProject/src/Main.java
@@ -3,5 +3,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Leslie Anne Sayin");
 		System.out.println("Nikki Nicholas Romero");
+		System.out.println("Kira");
 	}
 }
